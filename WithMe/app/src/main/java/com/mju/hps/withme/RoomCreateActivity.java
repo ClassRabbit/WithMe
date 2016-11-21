6bf246f384d39dd1a380ff7997f4c7ad777c6511
김한철 <Mac@gimhancheolui-MacBook-Pro.local>
2016-11-21 15:44:17 +0900

    fab 버튼 액티비티 적용

---
+++ WithMe/app/src/main/java/com/mju/hps/withme/RoomCreateActivity.java
@@ -0,0 +1,16 @@
+package com.mju.hps.withme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_create);
+    }
+
+    public void create_room() {
+    }
+}
