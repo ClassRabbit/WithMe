13799706c419eb644020cea6258b59383e0986a9
김한철 <Mac@gimhancheolui-MacBook-Pro.local>
2016-11-27 15:26:54 +0900

    Login view & 속성 변경

---
+++ WithMe/app/src/main/java/com/mju/hps/withme/RoomDetailActivity.java
@@ -0,0 +1,13 @@
+package com.mju.hps.withme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_detail);
+    }
+}
