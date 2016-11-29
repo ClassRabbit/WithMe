6eaf0f237b4c91e4d31f3c55d74f93d4b99cdae6
Minchan Kim <livingkmc@gmail.com>
2016-11-30 00:59:41 +0900

    main view 수정

--- WithMe/app/src/main/java/com/mju/hps/withme/RoomListAdapter.java
+++ WithMe/app/src/main/java/com/mju/hps/withme/RoomListAdapter.java
@@ -1,6 +1,8 @@
 package com.mju.hps.withme;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +12,13 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.mju.hps.withme.constants.Constants;
+import com.mju.hps.withme.database.DatabaseLab;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -40,23 +49,66 @@ public class RoomListAdapter extends BaseAdapter{
         // "listview_item" Layout을 inflate하여 convertView 참조 획득.
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.room_list_item, parent, false);
+            convertView = inflater.inflate(R.layout.item_mainview, parent, false);
         }
 
         // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-        ImageView iconImageView = (ImageView) convertView.findViewById(R.id.imageView1) ;
-        TextView titleTextView = (TextView) convertView.findViewById(R.id.textView1) ;
-        TextView descTextView = (TextView) convertView.findViewById(R.id.textView2) ;
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.item_main_image_view) ;
+        TextView titleTextView = (TextView) convertView.findViewById(R.id.item_main_text_view) ;
+        TextView addressTextView = (TextView) convertView.findViewById(R.id.item_main_text_view2) ;
+//        TextView descTextView = (TextView) convertView.findViewById(R.id.textView2) ;
 
         // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
         RoomItem roomItem = roomList.get(position);
 
         // 아이템 내 각 위젯에 데이터 반영
-//        iconImageView.setImageDrawable(roomItem.getIcon());
         titleTextView.setText(roomItem.getTitle());
-        descTextView.setText(roomItem.getAddress());
+        addressTextView.setText(roomItem.getAddress());
+//        descTextView.setText(roomItem.getAddress());
+        getRoomImage(roomItem.getId(), imageView);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
         return convertView;
+    }
+
+    private Bitmap profileImage;
+    private Boolean profileImageUploadCheck = false;
+    public void getRoomImage(String id, ImageView imageView){
+        final String baseShoppingURL = Constants.SERVER_URL + "/images/room/" + id +"/0.png";
+        Log.e("ImageURL", baseShoppingURL);
+        Thread mThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(baseShoppingURL); // URL 주소를 이용해서 URL 객체 생성
+                    //  아래 코드는 웹에서 이미지를 가져온 뒤
+                    //  이미지 뷰에 지정할 Bitmap을 생성하는   
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    conn.setDoInput(true);
+                    conn.connect();
+                    InputStream is = conn.getInputStream();
+                    profileImage = BitmapFactory.decodeStream(is);
+                    profileImageUploadCheck = true;
+                }
+                catch(IOException ex) {
+                    profileImageUploadCheck = false;
+                    Log.e("사진 읽어오기 실패", ex.toString());
+                }
+            }
+        };
+        mThread.start(); // 웹에서 이미지를 가져오는 작업 스레드 실행.
+        try {
+            mThread.join();
+            if(profileImageUploadCheck){
+                imageView.setImageBitmap(profileImage);
+            }
+            else {
+                imageView.setImageResource(R.drawable.user_information);
+            }
+        } catch (InterruptedException e) {
+
+        }
     }
 
     // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
